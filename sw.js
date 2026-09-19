a7e51a5c340ff6b48299a9fa9d35e5ee009ac6cc
marcelolisboa-eng <marcelobelezinha@gmail.com>
2026-09-19 13:30:19 -0300

    Atualizar cache v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='lisboa-os-v8';
+const C='lisboa-os-v9';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./logo3.png'])))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
