ef7f374152bfed60f055f4a7e4373cffebe1c51c
marcelolisboa-eng <marcelobelezinha@gmail.com>
2026-09-19 13:26:01 -0300

    Atualizar cache para logo3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='lisboa-os-v6';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./logo2.png'])))});
+const C='lisboa-os-v7';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./logo3.png'])))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).then(r=>{let x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request)));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
